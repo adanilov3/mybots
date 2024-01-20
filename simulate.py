72f54279ff29a6e95a91aee7690f86afd7d7e2ad
adanilov <alexandra.danilov@uvm.edu>
2024-01-20 15:36:34 -0500

    simulation window stays open

--- simulate.py
+++ simulate.py
@@ -1,4 +1,12 @@
 import pybullet as p
+import time
 
 physicsClient = p.connect(p.GUI)
+
+simulationTime = 1000
+for i in range(simulationTime):
+    p.stepSimulation()
+    time.sleep(1/60)
+    print(i)
+
 p.disconnect()
