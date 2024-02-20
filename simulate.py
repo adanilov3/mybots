c62d523b861b778b1bd134f29001b74a72fb8b38
adanilov <alexandra.danilov@uvm.edu>
2024-02-20 13:17:19 -0500

    tip toes

--- simulate.py
+++ simulate.py
@@ -7,12 +7,14 @@ import numpy
 physicsClient = p.connect(p.GUI)
 p.setAdditionalSearchPath(pybullet_data.getDataPath())
 
-simulationTime = 100
-p.setGravity(0,0,-9.8)
+simulationTime = 1000
+p.setGravity(0, 0, -9.8)
+
 planeId = p.loadURDF("plane.urdf")
 robotId = p.loadURDF("body.urdf")
 p.loadSDF("world.sdf")
 pyrosim.Prepare_To_Simulate(robotId)
+
 backLegSensorValues = numpy.zeros(simulationTime)
 frontLegSensorValues = numpy.zeros(simulationTime)
 
@@ -20,8 +22,11 @@ for i in range(simulationTime):
     p.stepSimulation()
     backLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg")
     frontLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("FrontLeg")
-    time.sleep(1/60)
-    #print(i)
+
+    pyrosim.Set_Motor_For_Joint(bodyIndex= robotId, jointName=b'Torso_BackLeg', controlMode= p.POSITION_CONTROL, targetPosition= -numpy.pi/4, maxForce= 500)
+    pyrosim.Set_Motor_For_Joint(bodyIndex= robotId, jointName=b'Torso_FrontLeg', controlMode= p.POSITION_CONTROL, targetPosition= numpy.pi/4, maxForce= 500)
+    time.sleep(1 / 60)
+    # print(i)
 
 numpy.save("data/backLegSensorValues.npy", backLegSensorValues)
 numpy.save("data/frontLegSensorValues.npy", frontLegSensorValues)
